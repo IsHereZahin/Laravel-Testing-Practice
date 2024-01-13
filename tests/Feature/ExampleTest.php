97021244b209a75be616b953fd4486c97992e2fa
zahin <abuzahinmohammadnowsin@gmail.com>
2024-01-13 17:24:40 +0600

    first test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,17 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
+        $response->assertSee('Documentation');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_dont_see_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertDontSee('This is a test');
+
         $response->assertStatus(200);
     }
 }
