f3c06341d2694da0f8c565d080d0c37b8b46aa38
zahin <abuzahinmohammadnowsin@gmail.com>
2024-02-05 17:33:43 +0600

    Parent SetUp in Test

---
+++ tests/Feature/ParentSetUpTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ParentSetUpTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    private User $user;
+    private User $admin;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = $this->createUser();
+        $this->admin = $this->createUser(isAdmin: true);
+    }
+
+    public function test_admin_can_see_add_new_product_button (): void
+    {
+        $response = $this->actingAs($this->admin)->get('/products');
+
+        // $response->assertStatus(200);
+        $response->assertOk();
+        $response->assertSee('Add new product');
+    }
+
+    public function test_user_can_not_see_add_new_product_button (): void
+    {
+        $response = $this->actingAs($this->user)->get('/products');
+
+        // $response->assertStatus(200);
+        $response->assertOk();
+        $response->assertDontSee('Add new product');
+    }
+
+    public function test_non_verifyed_user_can_not_access_products_page (): void
+    {
+        $response = $this->get('/products');
+        $response->assertStatus(302);
+    }
+
+    private function createUser(bool $isAdmin = false): User
+    {
+        return User::factory()->create([
+            'is_admin' => $isAdmin,
+        ]);
+    }
+}
